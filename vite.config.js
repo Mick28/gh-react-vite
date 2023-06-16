8ed53077b6f185545734a0a17d3850ea2b9e8323
Miguel Escurra <miguelescurra@gmail.com>
2023-06-16 12:23:46 -0300

    Fix bug Config base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://mick28.github.io/',
+  base: 'https://mick28.github.io/gh-react-vite/',
   plugins: [react()],
 })
